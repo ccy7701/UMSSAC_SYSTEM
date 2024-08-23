1873432db947328c11713fd0fb72ae955f27c7e5
Chiew Cheng Yi <chiewccy1@gmail.com>
2024-08-23 23:31:40 +0800

    Edit to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,38 +9,47 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
-<body class="welcome-background">
+<body class="welcome-body">
     <div class="container p-3">
         <div class="row align-items-center">
             <!-- Logo section -->
-            <div class="col-md-3 col-sm-2 col-xs-2">
-                <img src="{{ asset('images/UMSSACS_LOGO_FINAL.png') }}" alt="UMSSACS logo" class="img-fluid">
+            <div class="col-md-3 col-sm-4 col-xs-12 py-2 text-center">
+                <img src="{{ asset('images/UMSSACS_LOGO_FINAL.png') }}" alt="UMSSACS logo" class="welcome-website-logo img-fluid">
             </div>
             <!-- Links and buttons section -->
-            <div class="col-md-9 col-sm-2 col-xs-2 text-end">
-                <a href="#" class="text-decoration-none text-dark fw-bold px-3">Features</a>
-                <a href="#" class="text-decoration-none text-dark fw-bold px-3">Login</a>
-                <a href="#" class="btn btn-primary fw-bold px-3 mx-2">Get started</a>
+            <div class="welcome-nav col-md-9 col-sm-8 col-xs-12 py-2">
+                <a href="#" class="rsans text-decoration-none text-dark fw-bold px-3">Features</a>
+                <a href="#" class="rsans text-decoration-none text-dark fw-bold px-3">Login</a>
+                <a href="#" class="rsans btn btn-primary fw-bold px-3 mx-2">Get started</a>
             </div>
         </div>
     </div>
 
-    <!-- 
-    <div class="container">
-        <div class="row">
-            <div class="col-7 border">
-                <b>Hello world!</b>
-                <br>
-                <button type="button" class="btn btn-primary">PRIMARY</button>
-            </div>
-            <div class="col-5 border">
-                <b>I am using Bootstrap.</b>
-                <br>
-                <button type="button" class="btn btn-secondary">SECONDARY</button>
-            </div>
+    <br><br><br>
+
+    <div class="container px-6 mx-6">
+        <h1 class="rserif welcome-title fw-bold text-center">Enrich your student <br>life with UMSSACS</h1>
+        <p class="rserif welcome-subtitle text-center">UMSSACS is designed to help you keep track of your uni life.</p>
+    </div>
+
+    <br><br><br><br>
+
+    <div class="image-carousel px-sm-5">
+        <div class="carousel-track">
+            <img src="{{ asset('placeholder_images/welcome_img_1.png') }}" alt="Welcome image 1" class="img-fluid mx-3">
+            <img src="{{ asset('placeholder_images/welcome_img_2.png') }}" alt="Welcome image 2" class="img-fluid mx-3">
+            <img src="{{ asset('placeholder_images/welcome_img_3.png') }}" alt="Welcome image 3" class="img-fluid mx-3">
+            <img src="{{ asset('placeholder_images/welcome_img_4.png') }}" alt="Welcome image 4" class="img-fluid mx-3">
+            <img src="{{ asset('placeholder_images/welcome_img_5.png') }}" alt="Welcome image 5" class="img-fluid mx-3">
+            <img src="{{ asset('placeholder_images/welcome_img_6.png') }}" alt="Welcome image 6" class="img-fluid mx-3">
+            <img src="{{ asset('placeholder_images/welcome_img_1.png') }}" alt="Welcome image 1" class="img-fluid mx-3">
+            <img src="{{ asset('placeholder_images/welcome_img_2.png') }}" alt="Welcome image 2" class="img-fluid mx-3">
+            <img src="{{ asset('placeholder_images/welcome_img_3.png') }}" alt="Welcome image 3" class="img-fluid mx-3">
+            <img src="{{ asset('placeholder_images/welcome_img_4.png') }}" alt="Welcome image 4" class="img-fluid mx-3">
+            <img src="{{ asset('placeholder_images/welcome_img_5.png') }}" alt="Welcome image 5" class="img-fluid mx-3">
+            <img src="{{ asset('placeholder_images/welcome_img_6.png') }}" alt="Welcome image 6" class="img-fluid mx-3">
         </div>
     </div>
--->
 
     <!-- BOOTSTRAP JS -->
     <script src="resources/js/bootstrap.js"></script>
