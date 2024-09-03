e69d8dc0355e6595bf9824c3df3daf7905887a02
Chiew Cheng Yi <chiewccy1@gmail.com>
2024-09-03 14:52:56 +0800

    AcadProg: Some frontend

--- resources/views/acad-progress/progress-tracker.blade.php
+++ resources/views/acad-progress/progress-tracker.blade.php
@@ -17,6 +17,35 @@
 
     <div class="container p-3">
 
+        <div class="d-flex align-items-center">
+            <div class="acad-progress-section-header row w-100">
+                <div class="col-12 text-center">
+                    <h3 class="rserif fw-bold w-100 py-2">Academic progress tracker</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="row align-items-center py-3">
+
+            <div class="form-group mb-3">
+                <label for="select-semester" class="rsans form-label me-2">Select semester:</label>
+                <select id="select-semester" class="rsans form-select w-50" name="#">
+                    <option selected disabled value="">Choose...</option>
+                    <option value="S1-2021/2022">S1-2021/2022</option>
+                    <option value="S2-2021/2022">S2-2021/2022</option>
+                    <option value="S1-2022/2023">S1-2022/2023</option>
+                    <option value="S2-2022/2023">S2-2022/2023</option>
+                    <option value="S1-2023/2024">S1-2023/2024</option>
+                    <option value="S2-2023/2024">S2-2023/2024</option>
+                    <option value="S1-2024/2025">S1-2024/2025</option>
+                    <option value="S2-2024/2025">S2-2024/2025</option>
+                </select>
+            </div>
+
+            
+
+        </div>
+
     </div>
 
     <x-footer/>
