29b90f44dc90de0434d3bb56d8fcf3ffe16d2c6f
Chiew Cheng Yi <150312205+ccy7701@users.noreply.github.com>
2024-09-29 15:35:50 +0800

    Edited frontend for pages under 'auth'

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,8 @@
-<footer class="row w-100 mx-0 px-3 py-4" style="background-color: #EAF2FD">
-    <div class="col-6 text-start align-self-center">
-        <p class="rserif fs-4 fw-bold my-0">umssacs</p>
+<footer class="row w-100 mx-0 px-4 py-3" style="background-color: #EAF2FD">
+    <div class="col-6 col-xs-4 text-start align-self-center border">
+        <p id="footer-left" class="rserif fw-bold my-0">umssacs</p>
     </div>
-    <div class="col-6 text-end align-self-center">
-        <p class="rserif fs-6 my-0">Chiew Cheng Yi | BI21110236 | 2024</p>
+    <div class="col-6 col-xs-8 text-end align-self-center border">
+        <p id="footer-right" class="rserif my-0">Chiew Cheng Yi | BI21110236 | 2024</p>
     </div>
 </footer>
