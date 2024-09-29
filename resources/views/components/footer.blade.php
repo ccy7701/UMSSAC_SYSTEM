12a3a63c072c73fda1695bdfa51b54be2f6c2d45
Chiew Cheng Yi <150312205+ccy7701@users.noreply.github.com>
2024-09-29 13:10:41 +0800

    Edited frontend for Welcome, Features pages
    Also edited the footer component code.

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,12 +1,8 @@
-<footer class="w-100" style="background-color: #EAF2FD">
-    <div class="container px-5 py-4">
-        <div class="row">
-            <div class="col-6 text-start align-self-center">
-                <p class="rserif fs-4 fw-bold m-0">umssacs</p>
-            </div>
-            <div class="col-6 text-end align-self-center">
-                <p class="rserif fs-6 m-0">Chiew Cheng Yi | BI21110236 | 2024</p>
-            </div>
-        </div>
+<footer class="row w-100 mx-0 px-3 py-4" style="background-color: #EAF2FD">
+    <div class="col-6 text-start align-self-center">
+        <p class="rserif fs-4 fw-bold my-0">umssacs</p>
+    </div>
+    <div class="col-6 text-end align-self-center">
+        <p class="rserif fs-6 my-0">Chiew Cheng Yi | BI21110236 | 2024</p>
     </div>
 </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,59 +11,57 @@
 
 <body class="welcome-body">
     @vite('resources/js/app.js')
-    <div class="container p-3">
-        <div class="row align-items-center">
-            <!-- Logo section -->
-            <div class="col-md-3 col-sm-4 col-xs-12 py-2 text-center">
-                <a href="{{ route('welcome') }}">
-                    <img src="{{ asset('images/umssacs_logo_final.png') }}" alt="UMSSACS logo" class="welcome-website-logo img-fluid">
-                </a>
-            </div>
-            <!-- Links and buttons section -->
-            <div class="welcome-nav col-md-9 col-sm-8 col-xs-12 py-2">
-                <a href="{{ route('features') }}" class="rsans text-decoration-none text-dark fw-bold px-3">Features</a>
-                <a href="{{ route('login') }}" class="rsans text-decoration-none text-dark fw-bold px-3">Login</a>
-                <a href="{{ route('register') }}" class="rsans btn btn-primary fw-bold px-3 mx-2">Get started</a>
+    <main>
+        <div class="container p-3">
+            <div class="row align-items-center">
+                <!-- Logo section -->
+                <div class="col-md-3 col-sm-4 col-xs-12 py-2 text-center">
+                    <a href="{{ route('welcome') }}">
+                        <img src="{{ asset('images/umssacs_logo_final.png') }}" alt="UMSSACS logo" class="welcome-website-logo img-fluid">
+                    </a>
+                </div>
+                <!-- Links and buttons section -->
+                <div class="welcome-nav col-md-9 col-sm-8 col-xs-12 py-2">
+                    <a href="{{ route('features') }}" class="rsans text-decoration-none text-dark fw-bold px-3">Features</a>
+                    <a href="{{ route('login') }}" class="rsans text-decoration-none text-dark fw-bold px-3">Login</a>
+                    <a href="{{ route('register') }}" class="rsans btn btn-primary fw-bold px-3 mx-2">Get started</a>
+                </div>
             </div>
         </div>
-    </div>
-
-    <br><br><br>
-
-    <div class="container px-6 mx-6">
-        <h1 class="rserif welcome-title fw-bold text-center">Enrich your student<br>life with UMSSACS</h1>
-        <p class="rserif welcome-subtitle text-center">UMSSACS is designed to help you keep track of your uni life.</p>
-    </div>
-
-    <br><br><br><br>
-
-    <div class="image-carousel px-sm-5">
-        <div class="carousel-track">
-            <img src="{{ asset('images/study_partner_suggester.png') }}" alt="Study partner suggester placeholder illustration" class="img-fluid mx-3 border shadow">
-
-            <img src="{{ asset('placeholder-images/study_partner_list.png') }}" alt="Study partner list placeholder illustration" class="img-fluid mx-3 border shadow">
-
-            <img src="{{ asset('placeholder-images/bookmarked_events.png') }}" alt="Bookmarked events placeholder illustration" class="img-fluid mx-3 border shadow">
-
-            <img src="{{ asset('images/acad_prog_tracker.png') }}" alt="Academic progress tracker placeholder illustration" class="img-fluid mx-3 border shadow">
-
-            <img src="{{ asset('images/timetable_builder.png') }}" alt="Timetable builder placeholder illustration" class="img-fluid mx-3 border shadow">
-
-            <img src="{{ asset('images/system_users.png') }}" alt="System users placeholder illustration" class="img-fluid mx-3 border shadow">
-
-            <img src="{{ asset('images/study_partner_suggester.png') }}" alt="Study partner suggester placeholder illustration" class="img-fluid mx-3 border shadow">
-
-            <img src="{{ asset('placeholder-images/study_partner_list.png') }}" alt="Study partner list placeholder illustration" class="img-fluid mx-3 border shadow">
-
-            <img src="{{ asset('placeholder-images/bookmarked_events.png') }}" alt="Bookmarked events placeholder illustration" class="img-fluid mx-3 border shadow">
-
-            <img src="{{ asset('images/acad_prog_tracker.png') }}" alt="Academic progress tracker placeholder illustration" class="img-fluid mx-3 border shadow">
-
-            <img src="{{ asset('images/timetable_builder.png') }}" alt="Timetable builder placeholder illustration" class="img-fluid mx-3 border shadow">
-
-            <img src="{{ asset('images/system_users.png') }}" alt="System users placeholder illustration" class="img-fluid mx-3 border shadow">
+        <br><br>
+        <div class="container px-6 mx-6">
+            <h1 class="rserif welcome-title fw-bold text-center">Enrich your student<br>life with UMSSACS</h1>
+            <p class="rserif welcome-subtitle text-center">UMSSACS is designed to help you keep track of your uni life.</p>
         </div>
-    </div>
+        <br><br>
+        <div class="image-carousel px-sm-5">
+            <div class="carousel-track">
+                <img src="{{ asset('images/study_partner_suggester.png') }}" alt="Study partner suggester placeholder illustration" class="img-fluid mx-3 border shadow">
+    
+                <img src="{{ asset('placeholder-images/study_partner_list.png') }}" alt="Study partner list placeholder illustration" class="img-fluid mx-3 border shadow">
+    
+                <img src="{{ asset('placeholder-images/bookmarked_events.png') }}" alt="Bookmarked events placeholder illustration" class="img-fluid mx-3 border shadow">
+    
+                <img src="{{ asset('images/acad_prog_tracker.png') }}" alt="Academic progress tracker placeholder illustration" class="img-fluid mx-3 border shadow">
+    
+                <img src="{{ asset('images/timetable_builder.png') }}" alt="Timetable builder placeholder illustration" class="img-fluid mx-3 border shadow">
+    
+                <img src="{{ asset('images/system_users.png') }}" alt="System users placeholder illustration" class="img-fluid mx-3 border shadow">
+    
+                <img src="{{ asset('images/study_partner_suggester.png') }}" alt="Study partner suggester placeholder illustration" class="img-fluid mx-3 border shadow">
+    
+                <img src="{{ asset('placeholder-images/study_partner_list.png') }}" alt="Study partner list placeholder illustration" class="img-fluid mx-3 border shadow">
+    
+                <img src="{{ asset('placeholder-images/bookmarked_events.png') }}" alt="Bookmarked events placeholder illustration" class="img-fluid mx-3 border shadow">
+    
+                <img src="{{ asset('images/acad_prog_tracker.png') }}" alt="Academic progress tracker placeholder illustration" class="img-fluid mx-3 border shadow">
+    
+                <img src="{{ asset('images/timetable_builder.png') }}" alt="Timetable builder placeholder illustration" class="img-fluid mx-3 border shadow">
+    
+                <img src="{{ asset('images/system_users.png') }}" alt="System users placeholder illustration" class="img-fluid mx-3 border shadow">
+            </div>
+        </div>
+    </main>
 </body>
 
 </html>
