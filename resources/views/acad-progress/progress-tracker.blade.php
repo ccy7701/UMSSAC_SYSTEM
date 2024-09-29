766b18c8a838da30fc2d8a354e534df68c350e2f
Chiew Cheng Yi <150312205+ccy7701@users.noreply.github.com>
2024-09-29 20:49:09 +0800

    Edited frontend for acadprog

--- resources/views/acad-progress/progress-tracker.blade.php
+++ resources/views/acad-progress/progress-tracker.blade.php
@@ -23,25 +23,29 @@
         title="Success!"/>
     <br>
     <main class="flex-grow-1">
-        <div class="container p-3">
-            <div class="d-flex align-items-center">
-                <div class="section-header row w-100">
+        <!-- PAGE HEADER -->
+        <div class="row-container">
+            <div class="align-items-center px-3">
+                <div class="section-header row w-100 m-0 py-0 d-flex align-items-center">
                     <div class="col-12 text-center">
-                        <h3 class="rserif fw-bold w-100 py-2">Academic progress tracker</h3>
+                        <h3 class="rserif fw-bold py-2">Academic progress tracker</h3>
                     </div>
                 </div>
             </div>
-            <div class="row align-items-center py-3">
+        </div>
+        <br>
+        <!-- INITIALISER -->
+        <div class="row-container">
+            <div class="align-items-center px-3">
                 @if (profile()->profile_enrolment_session == '')
-                <div class="row py-3">
-                    <div class="d-flex justify-content-center align-items-center py-3 w-100 align-self-center">
-                        <div class="card shadow-sm w-80 p-3">
+                    <div class="d-flex justify-content-center align-items-center py-3 w-100">
+                        <div class="card shadow-sm w-90 p-3">
                             <div class="card-body">
                                 <h4 class="rserif card-title fw-bold pb-0">Settings things up...</h4>
                                 <p class="rsans card-text fs-5">Before we continue, please confirm the following details to set things up.</p>
                                 <br>
                                 <div class="w-100 d-flex justify-content-center align-items-center">
-                                    <form id="initialiser-fill-form" method="POST" action="{{ route('progress-tracker.initialise', ['profile_id' => profile()->profile_id]) }}" class="w-50">
+                                    <form id="initialiser-fill-form" method="POST" action="{{ route('progress-tracker.initialise', ['profile_id' => profile()->profile_id]) }}">
                                         @csrf
                                         <div class="rsans mb-3">
                                             <label for="select-enrolment-session" class="form-label fw-semibold">Enrolment session</label>
@@ -79,61 +83,58 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <!-- Submission confirmation modal -->
-                    <div class="rsans modal fade" id="submission-confirmation-modal" tabindex="-1" aria-labelledby="submissionConfirmationModalLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered">
-                            <div class="modal-content">
-                                <div class="modal-header py-2 d-flex align-items-center">
-                                    <p class="fw-semibold fs-5 mb-0">
-                                        Confirmation
-                                    </p>
-                                    <button type="button" class="btn-close ms-auto" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    Confirm if the details are correct before submission. They cannot be changed afterwards.
-                                </div>
-                                <div class="modal-footer">
-                                    <form id="initialiser-submit-form" method="POST" action="{{ route('progress-tracker.initialise', ['profile_id' => profile()->profile_id]) }}">
-                                        @csrf
-                                        <input type="hidden" id="received_profile_enrolment_sesion" name="profile_enrolment_session" value="">
-                                        <input type="hidden" id="received_course_duration" name="course_duration" value="">
-                                        <button type="button" class="btn btn-secondary fw-semibold me-1" data-bs-dismiss="modal">Wait, go back</button>
-                                        <button type="submit" class="btn btn-primary fw-semibold ms-1">OK, continue</button>
-                                    </form>
+                        <!-- Submission confirmation modal -->
+                        <div class="rsans modal fade" id="submission-confirmation-modal" tabindex="-1" aria-labelledby="submissionConfirmationModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header py-2 d-flex align-items-center">
+                                        <p class="fw-semibold fs-5 mb-0">
+                                            Confirmation
+                                        </p>
+                                        <button type="button" class="btn-close ms-auto" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Confirm if the details are correct before submission. They cannot be changed afterwards.
+                                    </div>
+                                    <div class="modal-footer">
+                                        <form id="initialiser-submit-form" method="POST" action="{{ route('progress-tracker.initialise', ['profile_id' => profile()->profile_id]) }}">
+                                            @csrf
+                                            <input type="hidden" id="received_profile_enrolment_sesion" name="profile_enrolment_session" value="">
+                                            <input type="hidden" id="received_course_duration" name="course_duration" value="">
+                                            <button type="button" class="btn btn-secondary fw-semibold me-1" data-bs-dismiss="modal">Wait, go back</button>
+                                            <button type="submit" class="btn btn-primary fw-semibold ms-1">OK, continue</button>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
                 @endif
-                @if (!profile()->profile_enrolment_session == '')
-                <!-- SEMESTER SELECTOR -->
-                <div class="form-group pb-3">
-                    <label for="select-semester" class="rsans form-label me-2">Select semester:</label>
-                    <select id="select-semester" class="rsans form-select w-50" name="#">
-                        <option selected disabled value="">Choose...</option>
-                        @foreach($all_semesters as $semester)
-                            <option value="{{ $semester->sem_prog_log_id }}">
-                                S{{ $semester->semester }}-{{ $semester->academic_session }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
-                
-                <!-- Send routes templates to external JS -->
-                <script>
-                    window.fetchBySemProgLogIdRoute = "{{ route('fetch-subject-stats', ['sem_prog_log_id' => ':sem_prog_log_id']) }}";
-                    window.getSubjectDataRouteTemplate = "{{ route('subject-stats-log.get', ['sem_prog_log_id' => ':sem_prog_log_id', 'subject_code' => ':subject_code']) }}";
-                    window.editSubjectRouteTemplate = "{{ route('subject-stats-log.edit', ['sem_prog_log_id' => ':sem_prog_log_id', 'subject_code' => ':subject_code']) }}";
-                    window.deleteRouteTemplate = "{{ route('subject-stats-log.delete', ['sem_prog_log_id' => ':sem_prog_log_id', 'subject_code' => ':subject_code']) }}";
-                    window.csrfToken = "{{ csrf_token() }}";
-                </script>
-    
-                <!-- RESULTS OVERVIEW -->
-                <div class="row py-3">
-                    <div class="d-flex justify-content-center align-items-center py-3 w-100 align-self-center">
-                        <div class="card shadow-sm w-80 p-3">
+                @if (profile()->profile_enrolment_session != '')
+                    <!-- SEMESTER SELECTOR -->
+                    <div class="form-group pb-3 px-3">
+                        <label for="select-semester" class="rsans form-label me-2">Select semester:</label>
+                        <select id="select-semester" class="rsans form-select w-50" name="#">
+                            <option selected disabled value="">Choose...</option>
+                            @foreach($all_semesters as $semester)
+                                <option value="{{ $semester->sem_prog_log_id }}">
+                                    S{{ $semester->semester }}-{{ $semester->academic_session }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <!-- Send routes templates to external JS -->
+                    <script>
+                        window.fetchBySemProgLogIdRoute = "{{ route('fetch-subject-stats', ['sem_prog_log_id' => ':sem_prog_log_id']) }}";
+                        window.getSubjectDataRouteTemplate = "{{ route('subject-stats-log.get', ['sem_prog_log_id' => ':sem_prog_log_id', 'subject_code' => ':subject_code']) }}";
+                        window.editSubjectRouteTemplate = "{{ route('subject-stats-log.edit', ['sem_prog_log_id' => ':sem_prog_log_id', 'subject_code' => ':subject_code']) }}";
+                        window.deleteRouteTemplate = "{{ route('subject-stats-log.delete', ['sem_prog_log_id' => ':sem_prog_log_id', 'subject_code' => ':subject_code']) }}";
+                        window.csrfToken = "{{ csrf_token() }}";
+                    </script>
+        
+                    <!-- RESULTS OVERVIEW SECTION -->
+                    <div class="row-container justify-content-center align-items-center align-self-center py-3">
+                        <div class="card shadow-sm p-3">
                             <div class="card-body">
                                 <h4 id="semester-results-title" class="rserif card-title fw-bold pb-0">My results at a glance</h4>
                                 <p class="rslab card-text fs-5">How are my results so far?</p>
@@ -141,7 +142,7 @@
                                 <div class="row">
                                     <!-- CGPA Section -->
                                     <div class="col-md-4 d-flex align-items-center">
-                                        <div>
+                                        <div class="py-1">
                                             <div class="d-flex">
                                                 <h2 class="rslab fs-5">CGPA up to this sem</h2>
                                                 <span><i class="align-self-center fa-regular fa-circle-question px-2" data-bs-toggle="tooltip" data-bs-placement="right" title="Cumulative Grade Point Average calculated up to this semester."></i></span>
@@ -151,7 +152,7 @@
                                         </div>
                                     </div>
                                     <div class="col-md-4 d-flex align-items-center">
-                                        <div>
+                                        <div class="py-1">
                                             <div class="d-flex">
                                                 <h2 class="rslab fs-5">SGPA</h2>
                                                 <span><i class="fa-regular fa-circle-question px-2" data-bs-toggle="tooltip" data-bs-placement="right" title="Semester Grade Point Average for this semester only."></i></span>
@@ -165,51 +166,48 @@
                             </div>
                         </div>
                     </div>
-                </div>
-                <!-- SUBJECTS TAKEN TABLE -->
-                <div class="row pb-3 pt-2">
-                    <div class="d-flex justify-content-center align-items-center py-3 w-100 align-self-center">
-                        <div class="card shadow-sm w-80 p-3">
+                    <!-- SUBJECTS TAKEN TABLE -->
+                    <div class="row-container justify-content-center align-items-center align-self-center py-3">
+                        <div class="card shadow-sm px-3">
                             <div class="card-body">
-                                <div class="d-flex align-items-center">
-                                    <div class="row w-100 px-3">
-                                        <div class="col-md-8 text-start">
-                                            <h4 class="rserif card-title fw-bold py-2 placeholder-glow">Subjects taken this semester</h4>
-                                        </div>
-                                        <div class="col-md-4 text-end pe-0">
-                                            <button id="add-subject-button" type="button" class="rsans btn btn-primary fw-bold w-50" data-bs-toggle="modal" data-bs-target="#add-subject-modal" disabled>Add subject</button>
-                                        </div>
-                                        <x-add-subject/>
-                                        <x-edit-subject/>
-                                        <x-delete-subject/>
-                                        <!-- Duplicate entry detected modal -->
-                                        <div class="rsans modal fade" id="duplicate-entry-modal" tabindex="-1" aria-labelledby="duplicateEntryModalLabel" aria-hidden="true">
-                                            <div class="modal-dialog modal-dialog-centered">
-                                                <div class="modal-content">
-                                                    <div class="modal-header py-2 d-flex align-items-center justify-content-center">
-                                                        <p class="fw-semibold fs-5 mb-0">
-                                                            Duplicate Entry Detected
-                                                        </p>
-                                                        <button type="button" class="btn-close ms-auto" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                    </div>
-                                                    <div class="modal-body">
-                                                        A subject with the same subject code already exists in the system. Please check your input again.
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <button type="button" class="btn btn-secondary fw-semibold" data-bs-dismiss="modal">Go back</button>
-                                                    </div>
+                                <div class="row align-items-center">
+                                    <div id="subcard-col-left" class="col-lg-8 col-12 mt-2">
+                                        <h4 class="rserif card-title fw-bold py-2 placeholder-glow">Subjects taken this semester</h4>
+                                    </div>
+                                    <div id="subcard-col-right" class="col-lg-4 col-12">
+                                        <button id="add-subject-button" type="button" class="rsans btn btn-primary fw-bold" data-bs-toggle="modal" data-bs-target="#add-subject-modal" disabled>Add subject</button>
+                                    </div>
+                                    <!-- MODALS -->
+                                    <x-add-subject/>
+                                    <x-edit-subject/>
+                                    <x-delete-subject/>
+                                    <!-- Duplicate entry detected modal -->
+                                    <div class="rsans modal fade" id="duplicate-entry-modal" tabindex="-1" aria-labelledby="duplicateEntryModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered">
+                                            <div class="modal-content">
+                                                <div class="modal-header py-2 d-flex align-items-center justify-content-center">
+                                                    <p class="fw-semibold fs-5 mb-0">
+                                                        Duplicate Entry Detected
+                                                    </p>
+                                                    <button type="button" class="btn-close ms-auto" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    A subject with the same subject code already exists in the system. Please check your input again.
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary fw-semibold" data-bs-dismiss="modal">Go back</button>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                                 <!-- SUBJECTS TAKEN TABLE -->
-                                <div class="table-responsive" style="overflow: visible;">
+                                <div class="table-responsive mt-3">
                                     <table class="rsans table table-hover">
                                         <thead class="fw-bold">
                                             <tr>
                                                 <th>Code</th>
-                                                <th>Subject name</th>
+                                                <th>Subject Name</th>
                                                 <th>Credit Hours</th>
                                                 <th>Grade</th>
                                                 <th>Grade Point</th>
@@ -224,10 +222,10 @@
                             </div>
                         </div>
                     </div>
-                </div>
                 @endif
             </div>
         </div>
+        <br>
     </main>
     <x-footer/>
 </body>
