30e7ffef36eb9a8d75e136c65c66c13c980fd01c
Chiew Cheng Yi <150312205+ccy7701@users.noreply.github.com>
2024-11-07 12:18:44 +0800

    Revision to DB schema to add new table
    Added new table 'club_creation_request' to handle all incoming requests for club creation (FM to admin).

---
+++ app/Models/ClubCreationRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ClubCreationRequest extends Model
+{
+    use HasFactory;
+
+    protected $table = 'club_creation_request';
+    protected $primaryKey = 'creation_request_id';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'club_name',
+        'club_category',
+        'club_description',
+        'club_image_paths',
+        'request_status'
+    ];
+
+    // ACCESSOR: Club image JSON attribute
+    public function getClubImagePathsAttribute() {
+        return $this->attributes['club_image_paths'];
+    }
+}
---
+++ database/migrations/2024_11_07_121226_create_club_creation_request_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('club_creation_request', function (Blueprint $table) {
+            $table->increments('creation_request_id');
+            $table->string('club_name', 512);
+            $table->string('club_category', 16);
+            $table->string('club_description', 1024);
+            $table->json('club_image_paths')->nullable();
+            $table->unsignedInteger('request_status')->comment('-1 - rejected, 0 - pending, 1 - approved, 2 - approved with amendments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('club_creation_request');
+    }
+};
